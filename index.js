669cd52f74b612ea21be53456627b8c016b188bd
pablo1022 <62977654+pablo1022@users.noreply.github.com>
2024-07-03 14:08:39 +0000

    E

--- index.js
+++ index.js
@@ -10,14 +10,28 @@ app.get('/oi', function (req, res) {
   res.send('Olá, mundo!')
 })
 
-const lista = ('rick sanchez','morty smith','summer smith')
+// Lista de Personagens
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
 
-// read all
+// Read All - [GET] /item
 app.get('/item', function (req, res) {
-
-  res.send('read all')
+  // Pegamos a lista e enviamos como resposta HTTP
+  res.send(lista)
 })
- 
 
-app.listen(3000)  
+// Sinalizamos para o Express que vamos usar JSON no Body
+app.use(express.json())
 
+// Create - [POST] /item
+app.post('/item', function (req, res) {
+  // Obtemos o nome enviado no Request Body
+  const item = req.body.nome
+
+  // Inserimos o item no final da lista
+  lista.push(item)
+
+  // Enviamos uma mensagem de sucesso!
+  res.send('Item criado com sucesso!')
+})
+
+app.listen(3000)
